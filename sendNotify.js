642f277f67d73b3d9ed1f03d38155b981f3335b1
HanSung <48339501+AutMan7@users.noreply.github.com>
2022-12-02 18:51:57 +0800

    Update sendNotify.js

--- sendNotify.js
+++ sendNotify.js
@@ -234,7 +234,7 @@ async function sendNotify(
   text,
   desp,
   params = {},
-  author = '\n\n本通知 By：https://github.com/whyour/qinglong',
+  author = '\n\n本通知 By：https://github.com/AutMan7',
 ) {
   //提供6种通知
   desp += author; //增加作者信息，防止被贩卖等
